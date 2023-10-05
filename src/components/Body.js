48123651fc601b9e7edcfa95a00c5afa7681a269
Param-21 <parambhatt21@gmail.com>
2023-10-05 19:27:38 +0530

    Episode-6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,30 @@
 import RestaurentCard from "./RestaurentCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import resList from "../utils/mockData";
+import shimmer from "./Shimmer";
 const Body  = () => {
 //Local variable-----> super powerful variable 
     const[listOfRestaurents,setListofRestaurents] = useState(resList); ///this is basically array de-structuring
+    
+    /*
+    useEffect(() => {
+        fetchData();
+
+    }, []); //with a callback function and another one is dependency array
+
+const fetchData = async () => {
+    const data  = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v6?lat-12.9351929&ing-77.62448069999999&page_type=DESKTOP_WEB_USTING"
+        );
+        const json = await data.json();
+        //Optional Chaining 
+
+        setListofRestaurents(json?.data.cards[2].data?.data?.cards);
+};*/
+
+    if (listOfRestaurents.length === 0) {
+        return <shimmer/>   
+    }
     return(
         <div className="Body">
             <div className="filter">
